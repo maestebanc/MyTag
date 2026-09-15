f50c09e50a61a2f08dad599b6e09b5110e972eed
Miguel Angel Esteban <maestebanc@gmail.com>
2026-09-15 23:28:05 +0200

    packaging: update Arch PKGBUILD checksum and build script for 0.50.0

--- packaging/build_arch.sh
+++ packaging/build_arch.sh
@@ -10,11 +10,8 @@ ARCH_DIR="$REPO_ROOT/packaging/arch"
 OUT_DIR="$REPO_ROOT/packaging/dist"
 
 cd "$ARCH_DIR"
-rm -rf src pkg
-if [ ! -f "$ARCH_DIR/mytag-0.50.0.tar.gz" ]; then
-  git -C "$REPO_ROOT" archive --format=tar.gz --prefix="MyTag-0.50.0/" HEAD -o "$ARCH_DIR/mytag-0.50.0.tar.gz"
-fi
-makepkg -f -d --clean --skipchecksums
+rm -rf src pkg ./*.tar.gz
+makepkg -f -d --clean
 
 mkdir -p "$OUT_DIR"
 cp ./*.pkg.tar.zst "$OUT_DIR/"
